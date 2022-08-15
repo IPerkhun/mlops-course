e4c1b68f4aa5e9da7f9b4b61e8ceb99b7ab5c341
Ivan_Perkhun <Lollol123>
2022-08-15 18:57:32 +0300

    add train.yaml

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Decrypt the file
-mkdir $HOME/secrets
+mkdir $PWD/secrets
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GDRIVE_SECRETS" \
---output $HOME/secrets/gdrive-mlops-course-secrets.json gdrive-mlops-course-secrets.json.gpg
+--output $PWD/secrets/gdrive-mlops-course-secrets.json gdrive-mlops-course-secrets.json.gpg
 
