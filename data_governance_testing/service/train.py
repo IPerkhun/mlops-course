a4ba7eff5efe177284c14d57da6e53898c124567
Ivan_Perkhun <Lollol123>
2022-08-16 01:15:36 +0300

    add dvc

--- data_governance_testing/service/train.py
+++ data_governance_testing/service/train.py
@@ -15,7 +15,7 @@ seed = 42
 ################################
 
 # Load in the data
-df = pd.read_csv("wine_quality.csv")
+df = pd.read_csv("volume/wine_quality.csv")
 
 # Split into train and test sections
 y = df.pop("quality")
@@ -35,7 +35,7 @@ train_score = regr.score(X_train, y_train) * 100
 test_score = regr.score(X_test, y_test) * 100
 
 # Write scores to a file
-with open("metrics.txt", 'w') as outfile:
+with open("outputs/metrics.txt", 'w') as outfile:
         outfile.write("Training variance explained: %2.1f%%\n" % train_score)
         outfile.write("Test variance explained: %2.1f%%\n" % test_score)
 
@@ -60,7 +60,7 @@ ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
 ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
 
 plt.tight_layout()
-plt.savefig("feature_importance.png",dpi=120) 
+plt.savefig("outputs/feature_importance.png",dpi=120) 
 plt.close()
 
 
@@ -84,5 +84,5 @@ plt.ylim((2.5,8.5))
 plt.xlim((2.5,8.5))
 
 plt.tight_layout()
-plt.savefig("residuals.png",dpi=120) 
+plt.savefig("outputs/residuals.png",dpi=120) 
 
